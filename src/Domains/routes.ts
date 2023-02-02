b25632bd5d763f1e663bc42ef4cc099098e27c69
tcardosoc <tcardosoc.dev@gmail.com>
2023-02-01 23:25:44 -0300

    feat: created project's routes file

---
+++ src/Domains/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const route = Router();
+
+export const Routes = [
+    // Differents routes,
+    route.get(
+        "/",
+        (req, res) => {
+            res.send("Running...")
+        }
+    )
+]
